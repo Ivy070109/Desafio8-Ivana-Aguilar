d9646cdb89a6229407069d2f242aebcaec547c09
Ivy070109 <Ivyaguilar07@gmail.com>
2024-01-09 18:30:44 -0300

    mejoras products

--- src/dao/models/carts.model.js
+++ src/dao/models/carts.model.js
@@ -1,22 +1,22 @@
 import mongoose from 'mongoose'
 //hay que importar el model de productos porque los vamos a utilizar para el populate
+import productModel from './products.model.js'
 
 mongoose.pluralize(null)
 
 const collection = "carts"
 
 const schema = new mongoose.Schema({
-    products: {
-        type: [ mongoose.Schema.Types.ObjectId ], ref: 'products'
-        //default: []
-    }
+    products: [
+        {
+            product: { 
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'products'
+            },
+            quantity: {
+                type: Number,
+            },
+            _id: false,
+        },
+    ],
 })
-
-//populate
-schema.pre('find', function(){
-    this.populate('products.product')
-})
-
-
-const model = mongoose.model(collection, schema)
-export default model
